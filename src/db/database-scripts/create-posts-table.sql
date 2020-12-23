7490b8e1bfa61549bb9369fc2dafce4c171b3c54
Tafir <vellinarius@yandex.ru>
2020-12-23 12:37:45 +0000

    CREATE db scripts

---
+++ src/db/database-scripts/create-posts-table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE posts(
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    user_id BIGSERIAL,
+    title VARCHAR(200) NOT NULL,
+    content VARCHAR(400),
+    date_created DATE NOT NULL,
+    date_updated DATE NOT NULL,
+    date_due DATE,
+    CONSTRAINT fk_user
+      FOREIGN KEY(user_id) 
+	  REFERENCES users(id)
+);
