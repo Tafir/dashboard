cb3525172c7d5ed8ed2aabc3141bda575adef264
Tafir <vellinarius@yandex.ru>
2021-01-19 17:47:13 +0000

    Changed from date to timestamp

--- src/db/database-scripts/create-posts-table.sql
+++ src/db/database-scripts/create-posts-table.sql
@@ -3,9 +3,9 @@ CREATE TABLE posts(
     user_id BIGSERIAL,
     title VARCHAR(200) NOT NULL,
     content VARCHAR(400),
-    date_created DATE NOT NULL,
-    date_updated DATE NOT NULL,
-    date_due DATE,
+    date_created TIMESTAMP WITH TIME ZONE,
+    date_updated TIMESTAMP WITH TIME ZONE,
+    date_due TIMESTAMP WITH TIME ZONE,
     category VARCHAR(10),
     CONSTRAINT fk_user
       FOREIGN KEY(user_id) 
